67b941120ca53131aba1b77954c181125e596e14
mortice-chrissparling <chris@mortice.io>
2021-10-30 17:19:40 +0100

    Removed address

--- openMAXIMS/master/Source Library/openmaxims_workspace/DomainObjects/src/ims/core/resource/place/domain/objects/Organisation.java
+++ openMAXIMS/master/Source Library/openmaxims_workspace/DomainObjects/src/ims/core/resource/place/domain/objects/Organisation.java
@@ -53,7 +53,7 @@ public class Organisation extends ims.domain.DomainObject implements ims.domain.
 	/** OrganisationName */
 	private String name;
 	/** Organisation Address */
-	private ims.core.generic.domain.objects.Address address;
+	//private ims.core.generic.domain.objects.Address address;
 	/** Organisation Type - SHA, NHS Trust, PCT, GP Practice, Health Boards, Indpendent Hospitals */
 	private ims.domain.lookups.LookupInstance type;
 	private Boolean isActive;
