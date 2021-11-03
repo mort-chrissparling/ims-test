4171cb1d7daca3b2a237774dc70c8c0a5dd9459a
mortice-chrissparling <chris@mortice.io>
2021-11-03 07:44:57 +0000

    Reset

--- openMAXIMS/master/Source Library/openmaxims_workspace/DomainObjects/src/ims/core/patient/domain/objects/PDSBatchTraceParams.java
+++ openMAXIMS/master/Source Library/openmaxims_workspace/DomainObjects/src/ims/core/patient/domain/objects/PDSBatchTraceParams.java
@@ -50,14 +50,14 @@ public class PDSBatchTraceParams extends ims.domain.DomainObject implements ims.
 		return true;
 	}
 
-	// /** New Style NHS NUmber */
-	// private String nHSNumber;
-	// /** Surname */
-	// private String surname;
-	// /** Forename */
-	// private String forename;
-	// /** Full Date of Birth */
-	// private java.util.Date dateOfBirth;
+	/** New Style NHS NUmber */
+	private String nHSNumber;
+	/** Surname */
+	private String surname;
+	/** Forename */
+	private String forename;
+	/** Full Date of Birth */
+	private java.util.Date dateOfBirth;
 	/** Gender */
 	private ims.domain.lookups.LookupInstance gender;
 	/** Date of Death or Date of Death range From */
